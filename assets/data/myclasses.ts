f14ecede2987abd3268ad25812b50e9fcd4c0f5f
alkayemm <52676416+alkayemm@users.noreply.github.com>
2019-07-17 09:42:06 -0500

    Add files via upload

---
+++ assets/data/myclasses.ts
@@ -0,0 +1,8 @@
+import {ClassInfo} from '../../app/classinfo';
+export const MYCLASSES: ClassInfo[] = 
+[
+    {cname:'SYST14444', room:'G407', dow:"Wed", tod:"8-11"},
+    {cname:'PROG89444', room:'G407', dow:"Wed", tod:"8-11"},
+    {cname:'INFO62344', room:'G407', dow:"Wed", tod:"8-11"}
+
+]
